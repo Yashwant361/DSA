82b87d9dc231435a81720cb68fc1eeca242b1bf4
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-08-04 05:39:56 +0000

    Array is Sorted Forward ,Backward or not all

--- Arrays/countOccurence.js
+++ Arrays/countOccurence.js
@@ -1,7 +1,7 @@
 let arr = [1, 3, 7, 8, 7, 5, 6, 7];
 
 let target = 7;
-let count = 0;
+let count =  0; // trace the target frequency
 
 for(let i = 0 ; i<= arr.length-1 ; i++){
     if(arr[i] === target){
--- Arrays/oddEvenArray.js
+++ Arrays/oddEvenArray.js
@@ -1,13 +1,13 @@
 let arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
 let evenArr = [];
-let oddArr = [];
+let oddArr  = [];
 
 // since we are not using .push(), we need to manually track where to insert each value.
-let oddIndex = 0;
-let evenIndex = 0;
+let oddIndex  = 0;
+let evenIndex = 0; 
 
- for(let i = 0 ; i< arr.length ; i++){
+ for(let i = 0 ; i < arr.length ; i++){
     if( arr[i] % 2 === 0){
         evenArr[evenIndex] = arr[i];
         evenIndex++;
