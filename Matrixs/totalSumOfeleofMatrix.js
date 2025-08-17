4a429cacfe065f658bdb615f142068417cae0ed6
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-08-17 04:07:57 +0000

    code to sum of a elements of matrix

---
+++ Matrixs/totalSumOfeleofMatrix.js
@@ -0,0 +1,19 @@
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+function totalSumOfMatrixEle(matrix){
+    let sum =0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            sum+=matrix[i][j];
+            
+        }
+        
+    }
+    return sum;
+}
+console.log(totalSumOfMatrixEle(matrix));
