e6d5c6919b9dd4603f7e60f7c6f4246d2fb303a2
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-08-17 06:39:42 +0000

    code to find maximum and minimum element of a matrix

--- Matrixs/printROwMinMax.js
+++ Matrixs/printROwMinMax.js
@@ -6,23 +6,32 @@ let matrix = [
 
 function rowMinMax(matrix){
     for (let i = 0; i < matrix.length; i++) {
-
-        //every new row starts fresh, with maxVal and minVal reset to the first element of that row.
-        let row = matrix[i]; // pick one row
-        let maxVal = row[0]; // assume first elements is Max
-        let minVal = row[0]; // assume first elements is Min
+        let row = matrix[i];
+        let maxVal = row[0];
+        let minVal = row[0];
 
         for (let j = 1; j < row.length; j++) {
-           if (row[j] > maxVal) {
-             maxVal = row[j]; // update max
-           }
-           if (row[j] < maxVal) {
-             minVal = row[j]; // update max
-           }
-            
+            if (row[j] > maxVal) maxVal = row[j];
+            if (row[j] < minVal) minVal = row[j];
         }
-        console.log(`Row ${i} -> Min = ${minVal} ,Max = ${maxVal}`);
-        
+        console.log(`Row ${i} -> Min = ${minVal} , Max = ${maxVal}`);
     }
 }
+
+
+function ColsMinMax(matrix){
+    for (let j = 0; j < matrix[0].length; j++) {
+        let maxVal = matrix[0][j];
+        let minVal = matrix[0][j];
+
+        for (let i = 1; i < matrix.length; i++) {
+            if (matrix[i][j] > maxVal) maxVal = matrix[i][j];
+            if (matrix[i][j] < minVal) minVal = matrix[i][j];
+        }
+        console.log(`Col ${j} -> Min = ${minVal} , Max = ${maxVal}`);
+    }
+}
+
 rowMinMax(matrix);
+console.log(`-------------------------`);
+ColsMinMax(matrix);
