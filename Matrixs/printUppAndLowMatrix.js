8f432b498351d932cbb392317625c31e893fa9e3
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-08-18 18:21:19 +0000

    Code to print to Upper matrix

---
+++ Matrixs/printUppAndLowMatrix.js
@@ -0,0 +1,23 @@
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+function printUpperLower(matrix) {
+
+    console.log("Upper Triangle:");
+   for (let i = 0; i <  matrix.length; i++) {
+        let row = "";
+        for (let j = 0; j <  matrix.length; j++) {
+            if (j >= i){
+                 row += matrix[i][j] + " ";
+            }
+            else row += "0 ";  // replace with 0 for clear triangle view
+        }
+        console.log(row);
+    }
+}
+
+printUpperLower(matrix);
+
