ba5e98392b70f4e85d3fa4c8cd9ee596fb60282d
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2025-09-26 17:33:10 +0000

    code to check sum of even digits is equals to sum of odd digits or not

---
+++ Functions/sum(Even)==sum(Odd).js
@@ -0,0 +1,30 @@
+
+function checkEvenAndOdd(num) {
+    let evenSum = 0;
+    let oddSum = 0;
+
+    while (num > 0) {
+        let rem = num % 10;
+        if (rem % 2 == 0) {
+            evenSum += rem;
+        }
+        else {
+            oddSum += rem;
+        }
+        num = Math.trunc(num / 10);
+    }
+    return evenSum === oddSum;g
+
+}
+
+function main() {
+    let num = 123456789;
+    let res = checkEvenAndOdd(num);
+    //  if(res){
+    //     console.log("yes");
+    //  }
+    //  else
+    //     console.log("no");
+    console.log(res);
+}
+main()
